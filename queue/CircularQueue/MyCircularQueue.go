8a26dca56c92e72c26418dda5e161ff97f8d6638
balloontmz <15111171986@163.com>
2019-03-07 14:19:20 +0800

    循环队列基本功能完成

--- queue/CircularQueue/MyCircularQueue.go
+++ queue/CircularQueue/MyCircularQueue.go
@@ -1,42 +1,91 @@
 package circularqueue
 
+import (
+	"errors"
+	"log"
+	"time"
+)
+
+// MyCircularQueue
 type MyCircularQueue struct {
-	Element interface{}
+	element  []string
+	Capacity int
+	front    int
+	rear     int
 }
 
-/** 在这里初始化你的数据结构，设置队列的大小。 */
+// Constructor 在这里初始化你的数据结构，设置队列的大小。
 func Constructor(k int) MyCircularQueue {
+	return MyCircularQueue{
+		element:  make([]string, k),
+		Capacity: k,
+		front:    0,
+		rear:     0,
+	}
+}
+
+// EnQueue ** 将一个元素插入循环队列，操作成功，返回 true  */
+func (q *MyCircularQueue) EnQueue(value string) bool {
+	if (q.rear+1)%q.Capacity == q.front%q.Capacity { // 这种方案采用的是为了区分开满时和空时状态减一个容量的做法
+		err := errors.New("队列已满！")
+		log.Print("队列已满，当前时间戳为：", time.Now().Unix(), err.Error())
+		return false
+	}
+	q.element[q.rear] = value
+	q.rear = (q.rear + 1) % q.Capacity
+	return true
 
 }
 
-/** 将一个元素插入循环队列，操作成功，返回 true  */
-func (this *MyCircularQueue) EnQueue(value int) bool {
-
+// DeQueue ** 从循环队列中删除一个元素。成功返回 该元素 */
+func (q *MyCircularQueue) DeQueue() string {
+	if q.rear == q.front {
+		err := errors.New("队列为空，没有数据出队列")
+		log.Print("队列为空，当前时间戳为：", time.Now().Unix(), err.Error())
+		return ""
+	}
+	res := q.element[q.front]
+	q.element[q.front] = ""
+	q.front = (q.front + 1) % q.Capacity
+	return res
 }
 
-/** Delete an element from the circular queue. Return true if the operation is successful. */
-func (this *MyCircularQueue) DeQueue() bool {
-
+// Front ** 获取队列的第一个元素。 */
+func (q *MyCircularQueue) Front() string {
+	if q.rear == q.front {
+		err := errors.New("队列为空，没有数据出队列")
+		log.Print("队列为空，当前时间戳为：", time.Now().Unix(), err.Error())
+		return ""
+	}
+	res := q.element[q.front]
+	return res
 }
 
-/** Get the front item from the queue. */
-func (this *MyCircularQueue) Front() int {
-
+// Rear ** 获取队列的最后一个元素。 */
+func (q *MyCircularQueue) Rear() string {
+	if q.rear == q.front {
+		err := errors.New("队列为空，没有数据出队列")
+		log.Print("队列为空，当前时间戳为：", time.Now().Unix(), err.Error())
+		return ""
+	}
+	res := q.element[(q.rear-1+q.Capacity)%q.Capacity]
+	return res
 }
 
-/** Get the last item from the queue. */
-func (this *MyCircularQueue) Rear() int {
-
+// IsEmpty ** 检查队列是否为空。 */
+func (q *MyCircularQueue) IsEmpty() bool {
+	if q.rear == q.front {
+		return true
+	}
+	return false
 }
 
-/** Checks whether the circular queue is empty or not. */
-func (this *MyCircularQueue) IsEmpty() bool {
-
-}
-
-/** Checks whether the circular queue is full or not. */
-func (this *MyCircularQueue) IsFull() bool {
-
+// IsFull ** 检查队列是否为满。 */
+func (q *MyCircularQueue) IsFull() bool {
+	if (q.rear+1)%q.Capacity == q.front%q.Capacity { // 这种方案采用的是为了区分开满时和空时状态减一个容量的做法
+		return true
+	}
+	return false
 }
 
 /**
