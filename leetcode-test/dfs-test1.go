cf5ddfcede5d4ef9881df90bc9d49ec7336c38bb
balloontmz <15111171986@163.com>
2019-03-18 13:42:06 +0800

    注释

--- leetcode-test/dfs-test1.go
+++ leetcode-test/dfs-test1.go
@@ -1,6 +1,7 @@
 package main
 
 // 一个简单的深度优先遍历练习
+// 暂时没实现树的赋值，所以遍历暂时没意义
 // 未完成
 
 // Node 单个节点的结构体
@@ -20,9 +21,9 @@ func dfs(node *Node, target int, mP *map[int]int) bool {
 	}
 	// 遍历当前节点的所有子节点
 	for _, val := range node.Next {
-		m := *mP // 取该地址的值
-		if _, ok := m[val.val]; !ok {
-			m[val.val] = 1
+		// m := *mP // 取该地址的值
+		if _, ok := (*mP)[val.val]; !ok {
+			(*mP)[val.val] = 1
 			if dfs(val, target, mP) == true {
 				return true
 			}
