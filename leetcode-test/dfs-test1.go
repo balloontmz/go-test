2f5feadc6d7e853c0b4ff81d2dbfbe7bd228f4b8
balloontmz <15111171986@163.com>
2019-03-18 12:57:18 +0800

    dfs 练习

---
+++ leetcode-test/dfs-test1.go
@@ -0,0 +1,32 @@
+package main
+
+// 一个简单的深度优先遍历练习
+// 未完成
+
+// Node 单个节点的结构体
+type Node struct {
+	val  int
+	Next []*Node
+}
+
+func main() {
+
+}
+
+func dfs(node *Node, target int, mP *map[int]int) bool {
+	// 查看当前递归的节点是否为指定目标
+	if node.val == target {
+		return true
+	}
+	// 遍历当前节点的所有子节点
+	for _, val := range node.Next {
+		m := *mP // 取该地址的值
+		if _, ok := m[val.val]; !ok {
+			m[val.val] = 1
+			if dfs(val, target, mP) == true {
+				return true
+			}
+		}
+	}
+	return false
+}
