b29d48b63a540f9b504dc5d8e9ac568e7348409c
NLE <liusilin@nle-tech.com>
2019-04-12 18:25:06 +0800

    快排通过，暂时没懂这段代码，可能需要再看下快排原理

--- sorter/src/algorithms/qsort/qsort.go
+++ sorter/src/algorithms/qsort/qsort.go
@@ -7,11 +7,12 @@ func quickSort(values []int, left, right int) {
 
 	// 快排，我擦 一个一个来的那种，基础排序，不是归并排序和希尔排序
 	for i <= j {
-		if j >= p && values[j] >= temp {
+		for j >= p && values[j] >= temp {
 			j--
 		}
 		if j >= p {
 			values[p] = values[j]
+			p = j // 之前忘了这一句，暂时没分析出作用--这一句也是，数组下标的浮动值
 		}
 
 		if values[i] <= temp && i <= p {
@@ -20,8 +21,10 @@ func quickSort(values []int, left, right int) {
 
 		if i <= p {
 			values[p] = values[i]
+			p = i // 之前忘了这一句，暂时没分析出作用
 		}
 	}
+	values[p] = temp // 也忘了这一句，依旧不知道什么意思
 	// 递归
 	if p-left > 1 {
 		quickSort(values, left, p-1)
