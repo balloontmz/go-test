93d660a31f35cc3da3f0134545733bc2c9f2265e
tomtiddler <tomtiddler@tomtiddlerdeMacBook-Pro.local>
2019-06-12 12:54:44 +0800

    数据库初始化，存在一些问题

--- go-web-programing/chitchat/app/data/setup.sql
+++ go-web-programing/chitchat/app/data/setup.sql
@@ -1,9 +1,20 @@
-
-
+-- Create a new database called 'chitchat'
+-- Connect to the 'mysql' database to run this snippet
+USE mysql 
+GO
+-- Create the new database if it does not exist already
+-- IF NOT EXISTS (
+--     SELECT name
+--         FROM sys.databases
+--         WHERE name = chitchat
+-- )
+CREATE DATABASE chitchat CHARSET=utf8 COLLATE=utf8_general_ci;
+GO
+ 
 -- Create a new table called 'users' in schema 'chitchat'
 -- Drop the table if it already exists
-IF OBJECT_ID('chitchat.users', 'U') IS NOT NULL
-DROP TABLE chitchat.users
+-- IF OBJECT_ID('chitchat.users', 'U') IS NOT NULL
+-- DROP TABLE chitchat.users
 GO
 -- Create the table in the specified schema
 CREATE TABLE chitchat.users
@@ -20,8 +31,8 @@ GO
 
 -- Create a new table called 'session' in schema 'chitchat'
 -- Drop the table if it already exists
-IF OBJECT_ID('chitchat.session', 'U') IS NOT NULL
-DROP TABLE chitchat.session
+-- IF OBJECT_ID('chitchat.session', 'U') IS NOT NULL
+-- DROP TABLE chitchat.session
 GO
 -- Create the table in the specified schema
 CREATE TABLE chitchat.session
@@ -37,8 +48,8 @@ GO
 
 -- Create a new table called 'threads' in schema 'chitchat'
 -- Drop the table if it already exists
-IF OBJECT_ID('chitchat.threads', 'U') IS NOT NULL
-DROP TABLE chitchat.threads
+-- IF OBJECT_ID('chitchat.threads', 'U') IS NOT NULL
+-- DROP TABLE chitchat.threads
 GO
 -- Create the table in the specified schema
 CREATE TABLE chitchat.threads
@@ -54,8 +65,8 @@ GO
 
 -- Create a new table called 'posts' in schema 'chitchat'
 -- Drop the table if it already exists
-IF OBJECT_ID('chitchat.posts', 'U') IS NOT NULL
-DROP TABLE chitchat.posts
+-- IF OBJECT_ID('chitchat.posts', 'U') IS NOT NULL
+-- DROP TABLE chitchat.posts
 GO
 -- Create the table in the specified schema
 CREATE TABLE chitchat.posts
