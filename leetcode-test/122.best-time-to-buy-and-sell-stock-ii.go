19baf6a6921f24a1bf736977660d7f3cf18f8b0c
tomtiddler <tomtiddler@tomtiddlerdeMacBook-Pro.local>
2019-08-02 18:46:02 +0800

    买卖股票的最大收益 2

---
+++ leetcode-test/122.best-time-to-buy-and-sell-stock-ii.go
@@ -0,0 +1,72 @@
+/*
+ * @lc app=leetcode id=122 lang=golang
+ *
+ * [122] Best Time to Buy and Sell Stock II
+ *
+ * https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/description/
+ *
+ * algorithms
+ * Easy (52.04%)
+ * Likes:    1141
+ * Dislikes: 1359
+ * Total Accepted:    351K
+ * Total Submissions: 667.2K
+ * Testcase Example:  '[7,1,5,3,6,4]'
+ *
+ * Say you have an array for which the i^th element is the price of a given
+ * stock on day i.
+ * 
+ * Design an algorithm to find the maximum profit. You may complete as many
+ * transactions as you like (i.e., buy one and sell one share of the stock
+ * multiple times).
+ * 
+ * Note: You may not engage in multiple transactions at the same time (i.e.,
+ * you must sell the stock before you buy again).
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: [7,1,5,3,6,4]
+ * Output: 7
+ * Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit
+ * = 5-1 = 4.
+ * Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 =
+ * 3.
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: [1,2,3,4,5]
+ * Output: 4
+ * Explanation: Buy on day 1 (price = 1) and sell on day 5 (price = 5), profit
+ * = 5-1 = 4.
+ * Note that you cannot buy on day 1, buy on day 2 and sell them later, as you
+ * are
+ * engaging multiple transactions at the same time. You must sell before buying
+ * again.
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: [7,6,4,3,1]
+ * Output: 0
+ * Explanation: In this case, no transaction is done, i.e. max profit = 0.
+ * 
+ */
+func maxProfit(prices []int) int {
+	// 买卖股票的最大收益 2
+	// 暂时没什么思路
+	// ✔ 201/201 cases passed (4 ms)
+	// ✔ Your runtime beats 93.17 % of golang submissions
+	// ✔ Your memory usage beats 64.52 % of golang submissions (3.1 MB)
+	res := 0
+	for index := 1; index < len(prices); index++ {
+		if prices[index] > prices[index-1] {
+			res = res + (prices[index] - prices[index-1])
+		}
+	}
+	return res
+}
+
