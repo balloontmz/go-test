de3b225d26718cb5cee4a606f0240ef4e8f5989b
tomtiddler <tomtiddler@tomtiddlerdeMacBook-Pro.local>
2019-08-21 14:06:02 +0800

    按平方数来分隔整数，bfs 版

---
+++ leetcode-test/279.perfect-squares.go
@@ -0,0 +1,86 @@
+/*
+ * @lc app=leetcode id=279 lang=golang
+ *
+ * [279] Perfect Squares
+ *
+ * https://leetcode.com/problems/perfect-squares/description/
+ *
+ * algorithms
+ * Medium (42.69%)
+ * Likes:    1627
+ * Dislikes: 136
+ * Total Accepted:    201.1K
+ * Total Submissions: 469.9K
+ * Testcase Example:  '12'
+ *
+ * Given a positive integer n, find the least number of perfect square numbers
+ * (for example, 1, 4, 9, 16, ...) which sum to n.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: n = 12
+ * Output: 3 
+ * Explanation: 12 = 4 + 4 + 4.
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: n = 13
+ * Output: 2
+ * Explanation: 13 = 4 + 9.
+ */
+ // 采用先进先出队列实现广度优先遍历
+func numSquares(n int) int {
+	// ✔ 588/588 cases passed (4 ms)
+	// ✔ Your runtime beats 97.63 % of golang submissions
+	// ✔ Your memory usage beats 33.33 % of golang submissions (6 MB)
+	var squares []int
+	squares = generateSquares(n) 
+	var queue []int
+	var marked = make([]bool, n+1)
+	queue = append(queue, n)
+
+	marked[n] = true
+	var level = 0
+	// 广度优先遍历 -- 当某次遍历成功时，该路径为最短路径
+	for len(queue) != 0 {
+		var size = len(queue)
+		level ++
+		for i := size; i > 0; i-- {
+			var cur = queue[0] // 后入先出
+			queue = queue[1:]
+			for _, v := range squares {
+				var next = cur - v
+				if next < 0 {
+					break
+				}
+				if next == 0 {
+					return level
+				}
+				if marked[next] == true {
+					continue
+				}
+				marked[next] = true
+				queue = append(queue, next)
+			}
+		}
+	}
+	// 整个遍历未成功
+	return n
+
+}
+
+// 此处平方数生成的算法可根据数学归纳法推算出来
+func generateSquares(n int) []int {
+	var squares []int
+	var square = 1
+	var diff = 3
+	for square <= n {
+		squares = append(squares, square)
+		square += diff
+		diff += 2
+	}
+	return squares
+}
+
