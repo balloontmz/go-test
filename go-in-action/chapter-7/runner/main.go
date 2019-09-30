b77e981a38b444ab7a476a00686e5e005192dafa
tomtiddler <tomtiddler@tomtiddlerdeMacBook-Pro.local>
2019-09-30 18:34:11 +0800

    runner 初步

---
+++ go-in-action/chapter-7/runner/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"log"
+	"os"
+	"time"
+
+	"runner/runner"
+)
+
+const timeout = 3 * time.Second
+
+/*
+代码清单 7-13 的第 16 行是 main 函数。在第 20 行，使用 timeout 作为超时时间传给 New 函数，并返回了一个指向 Runner 类型的指针。之后在第 23 行，
+使用 createTask 函数创建了 几个任务，并被加入 Runner 里。在第 42 行声明了 createTask 函数。这个函数创建的任务只 是休眠了一段时间，用来模拟正在进行工作。
+增加完任务后，在第 26 行调用了 Start 方法，main 函数会等待 Start 方法的返回。
+
+当 Start 返回时，会检查其返回的 error 接口值，并存入 err 变量。如果确实发生了错 误，代码会根据 err 变量的值来判断方法是由于超时终止的，还是由于收到了中断信号终止。
+ 如果没有错误，任务就是按时执行完成的。如果执行超时，程序就会用错误码 1 终止。如果接收 到中断信号，程序就会用错误码 2 终止。其他情况下，程序会使用错误码 0 正常终止。
+*/
+func main() {
+	log.Println("start work.")
+
+	// 为本次执行分配超时时间
+	r := runner.New(timeout)
+
+	// 加入要执行的任务
+	r.Add(createTask(), createTask(), createTask())
+
+	// 执行任务并处理结果
+	if err := r.Start(); err != nil {
+		switch err {
+		case runner.ErrTimeout:
+			log.Println("Terminationg due to timeout.")
+			os.Exit(1)
+		case runner.ErrInterrupt:
+			log.Println("Terminating due to interrupt.")
+			os.Exit(2)
+		}
+	}
+}
+
+func createTask() func(int) {
+	return func(id int) {
+		log.Printf("Processor - Task #%d", id)
+		time.Sleep(time.Duration(id) * time.Second)
+	}
+}
---
+++ go-in-action/chapter-7/runner/runner/runner.go
@@ -0,0 +1,90 @@
+//Package runner 包管理处理任务的运行和声明周期
+package runner
+
+import (
+	"errors"
+	"os"
+	"os/signal"
+	"time"
+)
+
+//Runner 在给定的超时时间内执行一组任务，并在操作系统发送中断信号时结束这些任务
+type Runner struct {
+
+	//interrupt 通道报告从操作系统发送的信号
+	interrupt chan os.Signal
+
+	//complete 通道报告处理任务已经完成
+	complete chan error
+
+	//timeout 报告处理任务已经超时
+	timeout <-chan time.Time
+
+	//tasks 持有一组以索引顺序执行的任务函数组
+	tasks []func(int)
+}
+
+//ErrTimeout 会在任务执行超时时返回
+var ErrTimeout = errors.New("received timeout")
+
+//ErrInterrupt 会在接收到操作系统的事件时返回
+var ErrInterrupt = errors.New("received interrupt")
+
+//New 返回一份新的准备使用的 Runner
+func New(d time.Duration) *Runner {
+	return &Runner{
+		interrupt: make(chan os.Signal, 1),
+		complete:  make(chan error),
+		timeout:   time.After(d),
+	}
+}
+
+//Add 将一个任务附加到 Runner 上。这个任务是一个接收一个 int 类型 ID 作为参数的函数
+func (r *Runner) Add(tasks ...func(int)) {
+	r.tasks = append(r.tasks, tasks...)
+}
+
+//Start 执行所有任务，并监视通道事件。
+func (r *Runner) Start() error {
+	//我们希望接收所有中断信号
+	signal.Notify(r.interrupt, os.Interrupt)
+
+	// 用不同的 goroutine 执行不同的任务
+	go func() {
+		r.complete <- r.run()
+	}()
+
+	select {
+	case err := <-r.complete:
+		return err
+	case <-r.timeout:
+		return ErrTimeout
+	}
+}
+
+func (r *Runner) run() error {
+	for id, task := range r.tasks {
+		// 检测操作系统的中断信号
+		if r.gotInterrupt() {
+			return ErrInterrupt
+		}
+
+		// 执行已注册的任务
+		task(id)
+	}
+	return nil
+}
+
+func (r *Runner) gotInterrupt() bool {
+	select {
+	// 当中断信号被触发时发出的信号
+	case <-r.interrupt:
+		//停止接收后续的任何信号
+		signal.Stop(r.interrupt)
+		return true
+
+		// 继续正常执行
+	default:
+		return false
+	}
+}
