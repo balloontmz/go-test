806538acd8b8471a7e7ec79051bd44997fa8520b
tomtiddler <tomtiddler@beardeMacBook-Pro.local>
2019-10-14 15:55:07 +0800

    组合

--- leetcode-test/77.combinations.go
+++ leetcode-test/77.combinations.go
@@ -15,10 +15,10 @@
  *
  * Given two integers n and k, return all possible combinations of k numbers
  * out of 1 ... n.
- * 
+ *
  * Example:
- * 
- * 
+ *
+ *
  * Input: n = 4, k = 2
  * Output:
  * [
@@ -29,17 +29,30 @@
  * ⁠ [1,3],
  * ⁠ [1,4],
  * ]
- * 
- * 
+ *
+ *
  */
 func combine(n int, k int) [][]int {
+	// 27/27 cases passed (140 ms)
+	// Your runtime beats 90.4 % of golang submissions
+	// Your memory usage beats 100 % of golang submissions (185.5 MB)
 	var combinations [][]int
 	var combineList []int
-	backtracking(combineList, combinations, 1, k, n)
+	backtracking(&combineList, &combinations, 1, k, n)
 	return combinations
 }
 
-func backtracking(combineList []int, combinations [][]int, start, k, n int)  {
-	
+func backtracking(combineList *[]int, combinations *[][]int, start, k, n int) {
+	if k == 0 {
+		var temp = make([]int, len(*combineList))
+		copy(temp, *combineList)
+		*combinations = append(*combinations, temp)
+		return
+	}
+	for i := start; i <= n-k+1; i++ {
+		*combineList = append(*combineList, i)
+		backtracking(combineList, combinations, i+1, k-1, n)
+		*combineList = (*combineList)[:len(*combineList)-1]
+	}
 }
 
