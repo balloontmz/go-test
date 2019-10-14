b07fc235a1482fa80f52ead45ec3b9ec2dc86f92
tomtiddler <tomtiddler@beardeMacBook-Pro.local>
2019-10-14 15:41:35 +0800

    含有相同元素求排列

--- leetcode-test/47.permutations-ii.go
+++ leetcode-test/47.permutations-ii.go
@@ -1,3 +1,5 @@
+import "sort"
+
 /*
  * @lc app=leetcode id=47 lang=golang
  *
@@ -15,10 +17,10 @@
  *
  * Given a collection of numbers that might contain duplicates, return all
  * possible unique permutations.
- * 
+ *
  * Example:
- * 
- * 
+ *
+ *
  * Input: [1,1,2]
  * Output:
  * [
@@ -26,12 +28,41 @@
  * ⁠ [1,2,1],
  * ⁠ [2,1,1]
  * ]
- * 
- * 
+ *
+ *
  */
 func permuteUnique(nums []int) [][]int {
+	// 30/30 cases passed (8 ms)
+	// Your runtime beats 95.28 % of golang submissions
+	// Your memory usage beats 50 % of golang submissions (8 MB)
 	var permutes [][]int
 	var permuteList []int
-	
+	sort.Ints(nums)
+	var hasVisited = make([]bool, len(nums))
+	backtracking(&permuteList, &permutes, hasVisited, nums)
+	return permutes
+}
+
+func backtracking(permuteList *[]int, permutes *[][]int, visited []bool, nums []int) {
+	if len(*permuteList) == len(nums) {
+		var temp = make([]int, len(*permuteList))
+		copy(temp, *permuteList)
+		*permutes = append(*permutes, temp)
+
+		return
+	}
+	for i := 0; i < len(visited); i++ {
+		if i != 0 && nums[i] == nums[i-1] && !(visited)[i-1] {
+			continue
+		}
+		if (visited)[i] {
+			continue
+		}
+		(visited)[i] = true
+		*permuteList = append(*permuteList, nums[i])
+		backtracking(permuteList, permutes, visited, nums)
+		*permuteList = (*permuteList)[:len(*permuteList)-1]
+		(visited)[i] = false
+	}
 }
 
