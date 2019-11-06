174bc4ebdf0f6a77b268bece8cb2d0c25839c59c
tomtiddler <balloontmz@gmail.com>
2019-11-06 12:30:49 +0800

    21.合并两个有序链表

---
+++ leetcode-test/21.merge-two-sorted-lists.go
@@ -0,0 +1,54 @@
+/*
+ * @lc app=leetcode id=21 lang=golang
+ *
+ * [21] Merge Two Sorted Lists
+ *
+ * https://leetcode.com/problems/merge-two-sorted-lists/description/
+ *
+ * algorithms
+ * Easy (49.71%)
+ * Likes:    2857
+ * Dislikes: 415
+ * Total Accepted:    735.3K
+ * Total Submissions: 1.5M
+ * Testcase Example:  '[1,2,4]\n[1,3,4]'
+ *
+ * Merge two sorted linked lists and return it as a new list. The new list
+ * should be made by splicing together the nodes of the first two lists.
+ *
+ * Example:
+ *
+ * Input: 1->2->4, 1->3->4
+ * Output: 1->1->2->3->4->4
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+//合并两个有序链表
+func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+	if l1 == nil {
+		return l2
+	}
+	if l2 == nil {
+		return l1
+	}
+
+	if l1.Val > l2.Val {
+		l2.Next = mergeTwoLists(l1, l2.Next)
+		return l2
+	} else {
+		l1.Next = mergeTwoLists(l1.Next, l2)
+		return l1
+	}
+}
+
+// @lc code=end
+
