532ebc2a2089d0f103daed0f73eb2f32fb3b44ec
tomtiddler <balloontmz@gmail.com>
2019-11-06 12:30:12 +0800

    83.递归删除链表中的重复元素

---
+++ leetcode-test/83.remove-duplicates-from-sorted-list.go
@@ -0,0 +1,59 @@
+/*
+ * @lc app=leetcode id=83 lang=golang
+ *
+ * [83] Remove Duplicates from Sorted List
+ *
+ * https://leetcode.com/problems/remove-duplicates-from-sorted-list/description/
+ *
+ * algorithms
+ * Easy (43.64%)
+ * Likes:    969
+ * Dislikes: 88
+ * Total Accepted:    378.1K
+ * Total Submissions: 866.1K
+ * Testcase Example:  '[1,1,2]'
+ *
+ * Given a sorted linked list, delete all duplicates such that each element
+ * appear only once.
+ *
+ * Example 1:
+ *
+ *
+ * Input: 1->1->2
+ * Output: 1->2
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: 1->1->2->3->3
+ * Output: 1->2->3
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+//递归删除链表中的重复元素
+func deleteDuplicates(head *ListNode) *ListNode {
+	// 165/165 cases passed (4 ms)
+	// Your runtime beats 88.33 % of golang submissions
+	// Your memory usage beats 31.25 % of golang submissions (3.2 MB)
+	if head == nil || head.Next == nil {
+		return head
+	}
+	head.Next = deleteDuplicates(head.Next)
+	if head.Val == head.Next.Val {
+		return head.Next
+	}
+	return head
+}
+
+// @lc code=end
+
