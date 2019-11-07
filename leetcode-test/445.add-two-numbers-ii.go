88cde2fcaddea92a459bdfc1b9e6913d02c3877a
tomtiddler <balloontmz@gmail.com>
2019-11-07 10:39:11 +0800

    445.计算两个链表的和--先取出所有链表值然后构造链表的方法

--- leetcode-test/445.add-two-numbers-ii.go
+++ leetcode-test/445.add-two-numbers-ii.go
@@ -42,106 +42,145 @@
  *     Next *ListNode
  * }
  */
-//采用链表反转以及删除链表的方法解两个链表的和--此处耗时较多，调试不够熟练。
+//计算两个链表的和
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-	// 1563/1563 cases passed (12 ms)
-	// Your runtime beats 69.23 % of golang submissions
-	// Your memory usage beats 100 % of golang submissions (5 MB)
-
-	// 涉及到 206 链表反转 以及 19 删除链表的倒数第n个节点
+	// 1563/1563 cases passed (20 ms)
+	// Your runtime beats 15.38 % of golang submissions
+	// Your memory usage beats 100 % of golang submissions (5.7 MB)
+	var l1Stack = buildStack(l1)
+	var l2Stack = buildStack(l2)
+	var tail = 0
 	var res = &ListNode{}
-	t1, t2, tail := getTail(l1, l2)
-	head, tailNum := addTwoNum(t1, t2, res)
-	tailR := reverseList(tail)
-
-	tempHead := tailR
-	//TODO: 垃圾硬编码！！！
-	for tempHead != nil || tailNum != 0 {
-		if tempHead == nil { // tailNum 不为0的情况下，只有初次
-			tempHead := &ListNode{}
-			tempHead.Val = tailNum
-			tailR = tempHead
-			tailNum = 0
-		} else {
-			if tailNum != 0 {
-				tempHead.Val += tailNum
-				if tempHead.Val >= 10 {
-					tailNum = tempHead.Val / 10
-					tempHead.Val %= 10
-
-					// 只有这种情况需要判断如果没有下一个则加一个下一个节点
-					if tempHead.Next == nil {
-						temp := &ListNode{}
-						tempHead.Next = temp
-					}
-				} else {
-					tailNum = 0
-				}
-			}
-			tempHead = tempHead.Next
+	var head = res
+	for len(l1Stack) != 0 || len(l2Stack) != 0 || tail != 0 {
+		var x, y int
+		if len(l1Stack) != 0 {
+			x = l1Stack[len(l1Stack)-1]
+			l1Stack = l1Stack[:len(l1Stack)-1]
 		}
+		if len(l2Stack) != 0 {
+			y = l2Stack[len(l2Stack)-1]
+			l2Stack = l2Stack[:len(l2Stack)-1]
+		}
+		sum := x + y + tail
+		var node = &ListNode{}
+		node.Val = sum % 10
+		node.Next = head.Next
+		head.Next = node
+		tail = sum / 10
 	}
-
-	head.Next = tailR
-
-	res = reverseList(res.Next)
-
-	return res
+	return res.Next
 }
 
-func addTwoNum(l1 *ListNode, l2 *ListNode, res *ListNode) (*ListNode, int) {
-	if l1 == nil {
-		return res, 0
+func buildStack(l *ListNode) []int {
+	var stack = []int{}
+	for l != nil {
+		stack = append(stack, l.Val)
+		l = l.Next
 	}
-	r, tailNum := addTwoNum(l1.Next, l2.Next, res)
-	var head = &ListNode{
-		(l1.Val + l2.Val + tailNum) % 10,
-		nil,
-	}
-	r.Next = head
-	num := (l1.Val + l2.Val + tailNum) / 10
-	return r.Next, num
+	return stack
 }
 
-func getTail(l1 *ListNode, l2 *ListNode) (*ListNode, *ListNode, *ListNode) {
-	var t1, t2 = l1, l2
-	for t1 != nil && t2 != nil {
-		t1 = t1.Next
-		t2 = t2.Next
-	}
-	if t1 != nil {
-		r1, r2 := removeNFromEnd(l1, t1)
-		return l2, r1, r2
-	}
-	if t2 != nil {
-		r1, r2 := removeNFromEnd(l2, t2)
-		return l1, r1, r2
-	}
-	return l1, l2, nil
-}
+//采用链表反转以及删除链表的方法解两个链表的和--此处耗时较多，调试不够熟练。 -- 题目不允许修改链表，次解法不符合要求！！！
+// func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+// 	// 1563/1563 cases passed (12 ms)
+// 	// Your runtime beats 69.23 % of golang submissions
+// 	// Your memory usage beats 100 % of golang submissions (5 MB)
 
-func removeNFromEnd(head *ListNode, fast *ListNode) (*ListNode, *ListNode) {
-	var slow = head
-	for fast.Next != nil {
-		fast = fast.Next
-		slow = slow.Next
-	}
-	culList := slow.Next
-	slow.Next = nil
-	return culList, head
-}
+// 	// 涉及到 206 链表反转 以及 19 删除链表的倒数第n个节点
+// 	var res = &ListNode{}
+// 	t1, t2, tail := getTail(l1, l2)
+// 	head, tailNum := addTwoNum(t1, t2, res)
+// 	tailR := reverseList(tail)
 
-func reverseList(head *ListNode) *ListNode {
-	var temp *ListNode
-	var next *ListNode
-	for head != nil {
-		next = head.Next
-		head.Next = temp
-		temp = head
-		head = next
-	}
-	return temp
-}
+// 	tempHead := tailR
+// 	//TODO: 垃圾硬编码！！！
+// 	for tempHead != nil || tailNum != 0 {
+// 		if tempHead == nil { // tailNum 不为0的情况下，只有初次
+// 			tempHead := &ListNode{}
+// 			tempHead.Val = tailNum
+// 			tailR = tempHead
+// 			tailNum = 0
+// 		} else {
+// 			if tailNum != 0 {
+// 				tempHead.Val += tailNum
+// 				if tempHead.Val >= 10 {
+// 					tailNum = tempHead.Val / 10
+// 					tempHead.Val %= 10
+
+// 					// 只有这种情况需要判断如果没有下一个则加一个下一个节点
+// 					if tempHead.Next == nil {
+// 						temp := &ListNode{}
+// 						tempHead.Next = temp
+// 					}
+// 				} else {
+// 					tailNum = 0
+// 				}
+// 			}
+// 			tempHead = tempHead.Next
+// 		}
+// 	}
+
+// 	head.Next = tailR
+
+// 	res = reverseList(res.Next)
+
+// 	return res
+// }
+
+// func addTwoNum(l1 *ListNode, l2 *ListNode, res *ListNode) (*ListNode, int) {
+// 	if l1 == nil {
+// 		return res, 0
+// 	}
+// 	r, tailNum := addTwoNum(l1.Next, l2.Next, res)
+// 	var head = &ListNode{
+// 		(l1.Val + l2.Val + tailNum) % 10,
+// 		nil,
+// 	}
+// 	r.Next = head
+// 	num := (l1.Val + l2.Val + tailNum) / 10
+// 	return r.Next, num
+// }
+
+// func getTail(l1 *ListNode, l2 *ListNode) (*ListNode, *ListNode, *ListNode) {
+// 	var t1, t2 = l1, l2
+// 	for t1 != nil && t2 != nil {
+// 		t1 = t1.Next
+// 		t2 = t2.Next
+// 	}
+// 	if t1 != nil {
+// 		r1, r2 := removeNFromEnd(l1, t1)
+// 		return l2, r1, r2
+// 	}
+// 	if t2 != nil {
+// 		r1, r2 := removeNFromEnd(l2, t2)
+// 		return l1, r1, r2
+// 	}
+// 	return l1, l2, nil
+// }
+
+// func removeNFromEnd(head *ListNode, fast *ListNode) (*ListNode, *ListNode) {
+// 	var slow = head
+// 	for fast.Next != nil {
+// 		fast = fast.Next
+// 		slow = slow.Next
+// 	}
+// 	culList := slow.Next
+// 	slow.Next = nil
+// 	return culList, head
+// }
+
+// func reverseList(head *ListNode) *ListNode {
+// 	var temp *ListNode
+// 	var next *ListNode
+// 	for head != nil {
+// 		next = head.Next
+// 		head.Next = temp
+// 		temp = head
+// 		head = next
+// 	}
+// 	return temp
+// }
 
 // @lc code=end
 
