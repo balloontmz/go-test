57a87b281d6b559f2a49ec7c58216670097a9490
tomtiddler <balloontmz@gmail.com>
2020-01-19 10:28:03 +0800

    459.优化

--- leetcode-test/459.repeated-substring-pattern.go
+++ leetcode-test/459.repeated-substring-pattern.go
@@ -51,16 +51,24 @@
 // 120/120 cases passed (352 ms)
 // Your runtime beats 11.92 % of golang submissions
 // Your memory usage beats 50 % of golang submissions (8 MB)
+//优化后
+// 120/120 cases passed (56 ms)
+// Your runtime beats 21.85 % of golang submissions
+// Your memory usage beats 50 % of golang submissions (6 MB)
 func repeatedSubstringPattern(s string) bool {
 	var n = len(s)
+
 	for i := n / 2; i > 0; i-- {
+		fmt.Print("当前遍历的 i 为:", i)
 		if n % i == 0 { // 只有 n 能整除 i 的情况下才可能组成长度相等的字符串
-			c := n / i // 重复次数
-			str := ""
-			for j := 0; j < c; j++ {
-				str += s[:i]
+			var tag bool
+			for j := 0; j < n / i; j++ {
+				if s[:i] != s[j * i : (j+1) * i] {
+					tag = true
+					break
+				}
 			}
-			if str == s {
+			if !tag {
 				return true
 			}
 		}
